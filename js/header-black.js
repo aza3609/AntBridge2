ecbafadb98a56807d131c704e404fe2a660d505f
aza3609 <89471930+aza3609@users.noreply.github.com>
2022-10-17 14:45:29 +0900

    header-js-update

--- js/header-black.js
+++ js/header-black.js
@@ -1,6 +1,7 @@
 const header = document.querySelector("header");
 const headerWhite = document.querySelector(".header--white");
 const headerHeight = header.getBoundingClientRect().height;
+const logo = header.querySelector(".header-logo img");
 const menuText = header.querySelectorAll(".header-nav_name");
 
 let lastScrollY = 0;
@@ -9,10 +10,16 @@ function handleHeaderScroll() {
   const scrollY = window.scrollY;
   if (scrollY > lastScrollY) {
     header.style.backgroundColor = "rgba(0, 0, 0, 1)";
+    logo.src = "src/ant-logo--white.png";
     header.classList.remove("hidden");
+    header.classList.remove("header--black");
+    header.classList.add("header--white");
   } else if (scrollY == 0) {
     header.style.backgroundColor = "rgba(0, 0, 0, 0)";
     header.classList.remove("hidden");
+    logo.src = "src/ant-logo.png";
+    header.classList.remove("header--white");
+    header.classList.add("header--black");
   } else {
     header.classList.add("hidden");
   }
