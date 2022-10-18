4700bedd74eb3c0d00b2d0efc9aa3917fd20d7bd
aza3609 <89471930+aza3609@users.noreply.github.com>
2022-10-18 09:42:18 +0900

    header-update

--- js/header-white.js
+++ js/header-white.js
@@ -2,6 +2,7 @@ const header = document.querySelector("header");
 const headerWhite = document.querySelector(".header--white");
 const headerHeight = header.getBoundingClientRect().height;
 const menuText = header.querySelectorAll(".header-nav_name");
+const logo = header.querySelector(".header-logo img");
 
 let lastScrollY = 0;
 
